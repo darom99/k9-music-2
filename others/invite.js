8f39f0e0dd161bec923859e33ee26553540625f1
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-12-05 17:08:51 +0300

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -6,7 +6,7 @@ const {
 } = require(`../config.json`);
 
 module.exports = {
-  name: `invite`,
+  name: `invitelllllll`,
   description: `Gives you an invite`,
   aliases: ["add"],
   cooldown: 3,
@@ -16,7 +16,7 @@ module.exports = {
     message.react("769665713124016128");
     //send the invite embed
     message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":heart: Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=769642999227351070&permissions=0&scope=bot")
-    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/769642999227351070/f1b78891507308fb76c0a66b56f4bcd6.webp?size=512"));
+    .setFooter("And enjoy listening to music!", ""));
 
   }
 }
