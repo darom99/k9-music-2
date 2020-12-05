7fa918c8cb72cbfac5cdb9cae7486c8492bc9317
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-12-05 18:29:10 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -23,10 +23,10 @@ Others
 
 ${prefix}\`help\`      ${prefix}\`ping\` 
 ${prefix}\`prefix\`    ${prefix}\`uptime\`
+
 Music
 
-\`${prefix}filter(fi)\`-\`${prefix}loop (l)\`-\`${prefix}lyrics (ly)\`
-\`${prefix}nowplaying(np)\`
+\`${prefix}filter(fi)\`-\`${prefix}loop (l)\`-\`${prefix}lyrics (ly)\`-\`${prefix}np\`
 \`${prefix}pause\`-\`${prefix}play(p)\`-\`${prefix}queue(qu)\`-\`${prefix}radio\`-\`${prefix}remove\`
 \`${prefix}resume (r)\`-\`${prefix}search\`-\`${prefix}shuffle(mix)\`-\`${prefix}skip(s)\`-\`${prefix}skipto(st)\`-\`${prefix}stop\`
 \`${prefix}volume(vol)\`
