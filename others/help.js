ecc242c0870d22ef987980f97ca803ebff84e9d6
robotmrcrow <73116672+robotmrcrow@users.noreply.github.com>
2020-12-05 17:30:31 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -13,16 +13,32 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setFooter(message.author.username, message.author.displayAvatarURL)
     .setColor('RANDOM')
-    .setTitle("**__bot help menu__**")
+    .setTitle("**__Help__**")
   .setImage( "https://cdn.discordapp.com/attachments/766626265448316938/782382346933501962/image0.gif")
   .setDescription(`**[  SUPPORT  ](https://discord.gg/ujsQgfe)**   -   **[   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=755768592104030208&permissions=1849163585&scope=bot)
 
+Others
 
+${prefix}\`help\`      ${prefix}\`ping\` 
+${prefix}\`prefix\`    ${prefix}\`uptime\`
 
+Music
 
+${prefix}\`filter\` (fi)          ${prefix}\`loop\` (l)
 
+${prefix}\`lyrics\`(ly)           ${prefix}nowplaying (np)
 
+${prefix}\`pause\`                ${prefix}\`play\` (p)
 
+${prefix}\`queue\` (qu)           ${prefix}\`radio\`
+
+${prefix}\`remove\´ (delete)      ${prefix}\`resume\` (r)
+
+${prefix}\`search\`               ${prefix}\`shuffle\` (mix)
+
+${prefix}\`skip\` (s)             ${prefix}\`skipto\` (st)
+
+${prefix}\`stop\`                  ${prefix}\`volume\` (vol)
 
 **`)
     
