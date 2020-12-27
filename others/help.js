9fbae5a9e1d34c83486cb5355505fc981c42b05f
darom99 <70956604+darom99@users.noreply.github.com>
2020-12-27 17:09:29 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -17,19 +17,19 @@ module.exports = {
   .setImage( "https://cdn.discordapp.com/attachments/792368491877498890/792512314620116992/image0.gif")
   .setDescription(`**[  SUPPORT  ](https://discord.gg/BgFvGtX5p8)**   -   **[   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=792369278103453709&permissions=3209984&scope=bot)
 
-prefix=\`${_}\`
+prefix=\`${prefix}\`
 
 Others
 
-\`${_}help\`      \`${_}ping\` 
-\`${_}prefix\`    \`${_}uptime\`
+\`${prefix}help\`      \`${prefix}ping\` 
+\`${prefix}prefix\`    \`${prefix}uptime\`
 
 Music
 
-\`${_}filter(fi)\`-\`${_}loop (l)\`-\`${_}lyrics (ly)\`-\`${_}np\`
-\`${_}pause\`-\`${_}play(p)\`-\`${_}queue(qu)\`-\`${_}radio\`-\`${_}remove\`
-\`${_}resume (r)\`-\`${_}search\`-\`${_}shuffle(mix)\`-\`${_}skip(s)\`-\`${_}skipto(st)\`-\`${_}stop\`
-\`${_}volume(vol)\`
+\`${prefix}filter(fi)\`-\`${prefix}loop (l)\`-\`${prefix}lyrics (ly)\`-\`${prefix}np\`
+\`${prefix}pause\`-\`${prefix}play(p)\`-\`${prefix}queue(qu)\`-\`${prefix}radio\`-\`${prefix}remove\`
+\`${prefix}resume (r)\`-\`${prefix}search\`-\`${prefix}shuffle(mix)\`-\`${prefix}skip(s)\`-\`${prefix}skipto(st)\`-\`${prefix}stop\`
+\`${prefix}volume(vol)\`
 
 
 __`)
